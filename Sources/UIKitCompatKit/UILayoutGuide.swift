0b9647296059e9ec12f57e65a2013aaa14bbd5af
JWI <jwi@j-w-i.org>
2025-10-02 22:45:01 +1000

    a

--- Sources/UIKitCompatKit/UILayoutGuide.swift
+++ Sources/UIKitCompatKit/UILayoutGuide.swift
@@ -16,9 +16,13 @@ public class UILayoutGuideShim {
     public func attach(to view: UIView) {
         owningView = view
         view.addSubview(proxyView)
+
+        // Default zero-size constraints so the proxy view doesn't interfere
+        proxyView.widthAnchor.constraint(equalToConstant: 0).isActive = true
+        proxyView.heightAnchor.constraint(equalToConstant: 0).isActive = true
     }
 
-    // MARK: - Anchors
+    // MARK: - Anchors using your Anchor type
     public var leadingAnchor: Anchor { proxyView.leadingAnchor }
     public var trailingAnchor: Anchor { proxyView.trailingAnchor }
     public var topAnchor: Anchor { proxyView.topAnchor }
@@ -29,16 +33,12 @@ public class UILayoutGuideShim {
     public var centerYAnchor: Anchor { proxyView.centerYAnchor }
 }
 
+@available(iOS, introduced: 6.0, obsoleted: 9.0)
+public typealias UILayoutGuide = UILayoutGuideShim
+
+@available(iOS, introduced: 6.0, obsoleted: 9.0)
 public extension UIView {
-    /// Mimics `addLayoutGuide(_:)` on iOS < 9 by attaching the UILayoutGuideShim's proxy view
-    @available(iOS, introduced: 6.0, obsoleted: 9.0)
     func addLayoutGuide(_ guide: UILayoutGuideShim) {
         guide.attach(to: self)
     }
 }
-
-@available(iOS, introduced: 6.0, obsoleted: 9.0)
-public typealias UILayoutGuide = UILayoutGuideShim
-
-
-
