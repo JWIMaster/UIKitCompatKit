0dd55cb9e8456f2ab4899fbb3f408899532e358a
JWI <jwi@j-w-i.org>
2025-10-02 21:25:13 +1000

    added an entire CGColorShim

--- Sources/UIKitCompatKit/CGColor.swift
+++ Sources/UIKitCompatKit/CGColor.swift
@@ -1,10 +1,30 @@
 import UIKit
+import CoreGraphics
 
-extension CGColor {
-    /// Mimics CGColor(red:green:blue:alpha:) on iOS <13
-    @available(iOS, introduced: 6.0, obsoleted: 13.0)
-    convenience init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
-        self.init(cgColor: color.cgColor)
+
+@available(iOS, introduced: 6.0, obsoleted: 13.0)
+public struct CGColorShim {
+    public let cgColor: CoreGraphics.CGColor
+
+    // Mimic the iOS 13 initializer
+    public init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        self.cgColor = UIColor(red: red, green: green, blue: blue, alpha: alpha).cgColor
+    }
+
+    // Existing CGColor properties
+    public var alpha: CGFloat { cgColor.alpha }
+    public var colorSpace: CGColorSpace? { cgColor.colorSpace }
+    public var components: [CGFloat]? { cgColor.components.map { Array($0) } }
+    public var numberOfComponents: Int { cgColor.numberOfComponents }
+    public var pattern: CGPattern? { cgColor.pattern }
+
+    // Forward equality
+    public static func == (lhs: CGColorShim, rhs: CGColorShim) -> Bool {
+        return lhs.cgColor == rhs.cgColor
     }
 }
+
+// Forward typealias
+@available(iOS, introduced: 6.0, obsoleted: 13.0)
+public typealias CGColor = CGColorShim
+
