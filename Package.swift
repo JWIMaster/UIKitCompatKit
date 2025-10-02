3026b43dba97d441675c196685035eaaf1a05699
JWI <jwi@j-w-i.org>
2025-10-02 18:33:03 +1000

    a

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 
 let package = Package(
     name: "UIKitCompatKit",
-    platforms: [
-        .iOS(.v6) // set minimum iOS you want to support
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
