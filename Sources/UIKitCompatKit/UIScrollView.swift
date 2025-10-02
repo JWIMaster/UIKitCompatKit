3c7e67b374ffe55090c55f9c6be947507fba02c5
JWI <jwi@j-w-i.org>
2025-10-02 22:03:51 +1000

    Split ScrollView and LayoutGuide

---
+++ Sources/UIKitCompatKit/UIScrollView.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+extension UIScrollView {
+    private struct Keys {
+        static var contentGuide = "contentLayoutGuideShim"
+        static var frameGuide = "frameLayoutGuideShim"
+    }
+
+    @available(iOS, introduced: 6.0, deprecated: 11.0)
+    public var contentLayoutGuide: UILayoutGuideShim {
+        if let guide = objc_getAssociatedObject(self, &Keys.contentGuide) as? UILayoutGuideShim {
+            return guide
+        }
+        let guide = UILayoutGuideShim()
+        guide.attach(to: self)
+        objc_setAssociatedObject(self, &Keys.contentGuide, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        return guide
+    }
+    
+    @available(iOS, introduced: 6.0, deprecated: 11.0)
+    public var frameLayoutGuide: UILayoutGuideShim {
+        if let guide = objc_getAssociatedObject(self, &Keys.frameGuide) as? UILayoutGuideShim {
+            return guide
+        }
+        let guide = UILayoutGuideShim()
+        guide.attach(to: self)
+        objc_setAssociatedObject(self, &Keys.frameGuide, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        return guide
+    }
+}
