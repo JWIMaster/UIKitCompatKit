f0e1dcc036ff416c382f3ef77e3e4f2bb62d77c5
JWI <jwi@j-w-i.org>
2025-10-04 14:17:36 +1000

    a

--- Sources/UIKitCompatKit/UILayoutGuide.swift
+++ Sources/UIKitCompatKit/UILayoutGuide.swift
@@ -16,40 +16,30 @@ public extension UIView {
     }
 }
 
-// MARK: - UILayoutGuide shim
+// MARK: - UILayoutGuideShim
 @available(iOS, introduced: 6.0, obsoleted: 9.0)
 public class UILayoutGuideShim {
     fileprivate weak var owningView: UIView?
-    fileprivate var topInset: CGFloat = 0
-    fileprivate var bottomInset: CGFloat = 0
-    fileprivate var leadingInset: CGFloat = 0
-    fileprivate var trailingInset: CGFloat = 0
-    
+    fileprivate var topConstant: CGFloat = 0
+    fileprivate var bottomConstant: CGFloat = 0
+    fileprivate var leadingConstant: CGFloat = 0
+    fileprivate var trailingConstant: CGFloat = 0
+
     public init() {}
-    
+
     fileprivate func attach(to view: UIView,
                             top: CGFloat = 0,
                             bottom: CGFloat = 0,
                             leading: CGFloat = 0,
                             trailing: CGFloat = 0) {
         owningView = view
-        topInset = top
-        bottomInset = bottom
-        leadingInset = leading
-        trailingInset = trailing
+        topConstant = top
+        bottomConstant = bottom
+        leadingConstant = leading
+        trailingConstant = trailing
     }
-    
-    public var layoutFrame: CGRect {
-        guard let view = owningView else { return .zero }
-        return CGRect(
-            x: view.bounds.origin.x + leadingInset,
-            y: view.bounds.origin.y + topInset,
-            width: view.bounds.width - leadingInset - trailingInset,
-            height: view.bounds.height - topInset - bottomInset
-        )
-    }
-    
-    // Anchors use your existing Anchor backport
+
+    // Dynamic anchors
     public var topAnchor: Anchor { Anchor(view: owningView!, attribute: .top) }
     public var bottomAnchor: Anchor { Anchor(view: owningView!, attribute: .bottom) }
     public var leadingAnchor: Anchor { Anchor(view: owningView!, attribute: .leading) }
@@ -60,59 +50,60 @@ public class UILayoutGuideShim {
     public var heightAnchor: Anchor { Anchor(view: owningView!, attribute: .height) }
     public var centerXAnchor: Anchor { Anchor(view: owningView!, attribute: .centerX) }
     public var centerYAnchor: Anchor { Anchor(view: owningView!, attribute: .centerY) }
+
+    // Computed layoutFrame (for compatibility)
+    public var layoutFrame: CGRect {
+        guard let view = owningView else { return .zero }
+        return CGRect(
+            x: view.bounds.minX + leadingConstant,
+            y: view.bounds.minY + topConstant,
+            width: view.bounds.width - leadingConstant - trailingConstant,
+            height: view.bounds.height - topConstant - bottomConstant
+        )
+    }
 }
 
 @available(iOS, introduced: 6.0, obsoleted: 9.0)
 public typealias UILayoutGuide = UILayoutGuideShim
 
-// MARK: - UIView extension for layout guides
-private var layoutGuidesKey: UInt8 = 0
+// MARK: - UIView layout guides
 private var layoutMarginsGuideKey: UInt8 = 0
 private var safeAreaGuideKey: UInt8 = 0
 
 @available(iOS, introduced: 6.0, obsoleted: 9.0)
 public extension UIView {
-    func addLayoutGuide(_ guide: UILayoutGuideShim) {
-        var guides = objc_getAssociatedObject(self, &layoutGuidesKey) as? [UILayoutGuide] ?? []
-        guides.append(guide)
-        objc_setAssociatedObject(self, &layoutGuidesKey, guides, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-    
-    var layoutGuides: [UILayoutGuide] {
-        get { objc_getAssociatedObject(self, &layoutGuidesKey) as? [UILayoutGuide] ?? [] }
-        set { objc_setAssociatedObject(self, &layoutGuidesKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
-    }
-    
-    // Layout margins guide
     var layoutMarginsGuide: UILayoutGuide {
         if let guide = objc_getAssociatedObject(self, &layoutMarginsGuideKey) as? UILayoutGuide { return guide }
         let guide = UILayoutGuide()
-        guide.attach(to: self,
-                     top: layoutMargins.top,
-                     bottom: layoutMargins.bottom,
-                     leading: layoutMargins.left,
-                     trailing: layoutMargins.right)
-        addLayoutGuide(guide)
+        guide.attach(
+            to: self,
+            top: layoutMargins.top,
+            bottom: layoutMargins.bottom,
+            leading: layoutMargins.left,
+            trailing: layoutMargins.right
+        )
         objc_setAssociatedObject(self, &layoutMarginsGuideKey, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return guide
     }
-    
-    // Safe area guide
+
     var safeAreaLayoutGuide: UILayoutGuide {
         if let guide = objc_getAssociatedObject(self, &safeAreaGuideKey) as? UILayoutGuide { return guide }
+
         let guide = UILayoutGuide()
-        var topInset: CGFloat = layoutMargins.top
-        var bottomInset: CGFloat = layoutMargins.bottom
+        var topInset: CGFloat = 0
+        var bottomInset: CGFloat = 0
+
         if let vc = sequence(first: self.next, next: { $0?.next }).compactMap({ $0 as? UIViewController }).first {
             topInset += vc.topLayoutGuide.length
             bottomInset += vc.bottomLayoutGuide.length
         }
+
+        // Attach guide to view, top/bottom include safe area offsets, leading/trailing = 0
         guide.attach(to: self,
                      top: topInset,
                      bottom: bottomInset,
-                     leading: layoutMargins.left,
-                     trailing: layoutMargins.right)
-        addLayoutGuide(guide)
+                     leading: 0,
+                     trailing: 0)
         objc_setAssociatedObject(self, &safeAreaGuideKey, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return guide
     }
@@ -136,10 +127,10 @@ public extension UIViewController {
         objc_setAssociatedObject(self, &topGuideKey, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return guide
     }
-    
+
     var bottomLayoutGuide: UILayoutSupportShim {
         if let guide = objc_getAssociatedObject(self, &bottomGuideKey) as? UILayoutSupportShim { return guide }
-        let guide = UILayoutSupportShim(length: 0) // default bottom
+        let guide = UILayoutSupportShim(length: 0)
         objc_setAssociatedObject(self, &bottomGuideKey, guide, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return guide
     }
