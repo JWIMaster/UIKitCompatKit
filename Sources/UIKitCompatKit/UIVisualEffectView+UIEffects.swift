df811ec577bd84460286fd73a36944038d4795af
JWI <jwi@j-w-i.org>
2025-10-07 21:47:40 +1100

    a

--- Sources/UIKitCompatKit/UIVisualEffectView+UIEffects.swift
+++ Sources/UIKitCompatKit/UIVisualEffectView+UIEffects.swift
@@ -52,7 +52,7 @@ public class UIVisualEffectView: UIView {
         case .a4:
             return 0.1
         case .a5:
-            return 0.25
+            return 0.1
         case .a6:
             return 0.35
         case .a7_a8:
