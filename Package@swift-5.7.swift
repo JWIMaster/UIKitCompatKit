21e3b0d962287b6224b8072b2c40a67dc2ee753d
JWI <jwi@j-w-i.org>
2025-12-04 17:28:37 +1100

    add a higher package version check for swift tools 5.7

---
+++ Package@swift-5.7.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "UIKitCompatKit",
+    platforms: [
+        .iOS("7.0")
+    ],
+    products: [
+        .library(
+            name: "UIKitCompatKit",
+            targets: ["UIKitCompatKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JWIMaster/IBPCollectionViewCompositionalLayout", branch: "master")
+    ],
+    targets: [
+        .target(
+            name: "UIKitCompatKit",
+            dependencies: ["OAStackView", "LiveFrost", "IBPCollectionViewCompositionalLayout"],
+            path: "Sources/UIKitCompatKit",
+            swiftSettings: [
+                .define("MODERN_BUILD")
+            ]
+        ),
+        .target(
+            name: "OAStackView",
+            path: "Sources/OAStackView",
+            publicHeadersPath: "."
+        ),
+        .target(
+            name: "LiveFrost",
+            path: "Sources/LiveFrost",
+            publicHeadersPath: "."
+        ),
+        .testTarget(
+            name: "UIKitCompatKitTests",
+            dependencies: ["UIKitCompatKit"]
+        ),
+    ]
+)
